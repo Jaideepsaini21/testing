07cae98a92df8ec5e6efd9fa479cbdab3288b78f
Jaideep Saini <151540057+Jaideepsaini21@users.noreply.github.com>
2025-06-13 12:43:42 +0530

    Update urls.py

--- SDASchool/urls.py
+++ SDASchool/urls.py
@@ -20,6 +20,5 @@ from django.urls import path,include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("my-school/", include('myschool.urls')),
-    path("task/",include('newtask.urls')),
 ]   
 
